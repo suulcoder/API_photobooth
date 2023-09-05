2cafec87206db02e0a83102516ff4db928e0bf7b
suulcoder <saulcontreras@Sauls-MacBook-Air.local>
2023-09-05 00:45:50 -0600

    done with sound

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip, CompositeAudioClip
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
+
 img_size = 200
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +14,7 @@ def image_to_video(path):
     size = (width,height)
     out = cv2.VideoWriter(path + '.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
     
-    for i in range(0,20):
+    for i in range(0,10):
         out.write(img)
     
     out.release()
@@ -60,19 +61,37 @@ def index():
             image_to_video(image3)
             image_to_video(image4)
             
+            _video1 = VideoFileClip(video1 + '.mp4')
+            _video2 = VideoFileClip(video2 + '.mp4')
+            _video3 = VideoFileClip(video3 + '.mp4')
+            _video4 = VideoFileClip(video4 + '.mp4')
+            
             final_clip = concatenate_videoclips(
                 [
-                    VideoFileClip(video1 + '.mp4'),
+                    _video1,
                     VideoFileClip(image1 + '.mp4'),
-                    VideoFileClip(video2 + '.mp4'),
+                    _video2,
                     VideoFileClip(image2 + '.mp4'),
-                    VideoFileClip(video3 + '.mp4'),
+                    _video3,
                     VideoFileClip(image3 + '.mp4'),
-                    VideoFileClip(video4 + '.mp4'),
+                    _video4,
                     VideoFileClip(image4 + '.mp4'),
                 ]
             )
-            final_clip.write_videofile("final.mp4")
+        
+            audio = AudioFileClip("audios/LMDA_PHOTOBOOTH.mp3").set_duration(final_clip.duration)
+            fx_1 = AudioFileClip("audios/photo_fx_1.mp3")
+            fx_2 = AudioFileClip("audios/photo_fx_2.mp3")
+            final_clip.audio = CompositeAudioClip(
+                [
+                    audio.set_start(0),
+                    fx_1.set_start(_video1.duration),
+                    fx_2.set_start(_video1.duration + _video2.duration + 1),
+                    fx_1.set_start(_video1.duration + _video2.duration + _video3.duration + 2),
+                    fx_2.set_start(_video1.duration + _video2.duration + _video3.duration + _video4.duration + 3)
+                ]
+            )
+            final_clip.write_videofile('final.mp4', codec="libx264", audio_codec="aac")
             
             print("done")
             return send_file("final.mp4", attachment_filename='final.mp4')
